27c3ef278c57d19a2b3d1c682906c7dc92bfce33
RageOfFire <buihongson453@gmail.com>
2022-03-28 16:09:50 +0700

    turn message to embed

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js");
 const request = require('request');
+const { MessageEmbed } = require('discord.js');
+const { hyperlink } = require('@discordjs/builders');
 require("dotenv").config()
 
 const client = new Discord.Client({
@@ -23,10 +25,10 @@ client.on("messageCreate", (message) => {
             method: 'POST',
             url: 'https://waifu.p.rapidapi.com/path',
             qs: {
-                user_id: `<@${message.author.id}>`,
-                message: `${message.content.slice(prefix.length)}`,
-                from_name: `<@${message.author.username}>`,
-                to_name: `<@${client.user.username}>`,
+                user_id: message.author.id,
+                message: message.content.slice(prefix.length),
+                from_name: message.author.toString(),
+                to_name: client.user.toString(),
                 situation: 'Kora đang nói chuyện với bạn',
                 translate_from: 'en vn',
                 translate_to: 'vn'
@@ -41,15 +43,37 @@ client.on("messageCreate", (message) => {
             json: true
         };
 
-        request(options, function(error, response, body) {
+        request(options, function (error, response, body) {
             if (error) throw new Error(error);
+            const koraEmbed = new MessageEmbed()
+                .setColor('#faa152')
+                .setTitle('Kora')
+                .setURL('https://www.crystalgem.cf/')
+                .setAuthor({ name: 'Kora', iconURL: 'https://cdn.discordapp.com/avatars/951682890297659412/7e31923b9f673ca23c66336b2a97bead.webp?size=160', url: 'https://www.crystalgem.cf/' })
+                .setDescription(body)
+                .setThumbnail('https://cdn.discordapp.com/avatars/951682890297659412/7e31923b9f673ca23c66336b2a97bead.webp?size=160')
+                .addFields(
+                    { name: 'Đang trò chuyện với', value: `${message.author.toString()}` },
+                )
+                .setTimestamp()
+                .setFooter({ text: 'Kora', iconURL: 'https://cdn.discordapp.com/avatars/951682890297659412/7e31923b9f673ca23c66336b2a97bead.webp?size=160' });
 
-            message.reply(body);
+            message.reply({ embeds: [koraEmbed] });
         });
     }
     if (message.content.includes("@here") || message.content.includes("@everyone")) return false;
     if (message.mentions.has(client.user.id) && message.type !== "REPLY") {
-        message.reply("Này bạn ping tôi à? Lỡ ping rồi thì chơi game này đi cực hay luôn đó là CrystalGem thử xem tại đây? https://crystalgem.cf/\nBất kì khi nào rảnh quay lại nói chuyện với tôi qua k-chat nha.")
+        const adEmbed = new MessageEmbed()
+        .setColor('#faa152')
+        .setTitle('Kora')
+        .setURL('https://www.crystalgem.cf/')
+        .setAuthor({ name: 'Kora', iconURL: 'https://cdn.discordapp.com/avatars/951682890297659412/7e31923b9f673ca23c66336b2a97bead.webp?size=160', url: 'https://www.crystalgem.cf/' })
+        .setDescription(`Này bạn ping tôi à? Lỡ ping rồi thì chơi game này đi cực hay luôn đó là CrystalGem thử xem ${hyperlink("tại đây","https://crystalgem.cf/")} ?\nBất kì khi nào rảnh quay lại nói chuyện với tôi qua k-(nói chuyện) nha.`)
+        .setThumbnail('https://cdn.discordapp.com/avatars/951682890297659412/7e31923b9f673ca23c66336b2a97bead.webp?size=160')
+        .setTimestamp()
+        .setFooter({ text: 'Kora', iconURL: 'https://cdn.discordapp.com/avatars/951682890297659412/7e31923b9f673ca23c66336b2a97bead.webp?size=160' });
+
+    message.reply({ embeds: [adEmbed] });
     }
 })
 client.login(process.env.TOKEN)
