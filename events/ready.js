6c3401011301ce0a177b3d2776adec2655e46204
RageOfFire <buihongson453@gmail.com>
2022-05-26 09:41:11 +0700

    update activity

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,10 @@
 module.exports = {
     name: "ready",
     run: async(bot) => {
+        bot.client.user.setActivity("CrystalGem", {
+            type: "PLAYING",
+            url: "https://www.crystalgem.cf/"
+        });
         console.log("logged as " + bot.client.user.tag)
     }
 }
