2fbda1f0883912dad8d5e6eaff277319f52bf1bc
RageOfFire <buihongson453@gmail.com>
2023-04-21 22:23:12 +0700

    limit for api using

--- util/apiv2.js
+++ util/apiv2.js
@@ -16,12 +16,12 @@ Kora: You can download it in here: https://rageoffire.itch.io/crystalgem`;
       openai.createCompletion({
         model: "text-ada-001", // cheapest api for better and expensive using "text-davinci-003"
         prompt: prompt,
-        temperature: 0.7,
-        max_tokens: 2048, // Limit this for money cost
+        temperature: 0.9,
+        max_tokens: 1989, // Limit this for money cost
         top_p: 1.0,
         frequency_penalty: 0,
-        presence_penalty: 0,
-        stop: ["You:"],
+        presence_penalty: 0.6,
+        stop: ["Kora: ", "You:"],
       })
       .then(response => {
         prompt += `${response.data.choices[0].text}\n`;
