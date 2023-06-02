f5546d42fe7c03a63f88106e660a59dc98583034
RageOfFire <buihongson453@gmail.com>
2023-06-02 21:54:36 +0700

    back to old api

--- commands/talk/chat.js
+++ commands/talk/chat.js
@@ -1,6 +1,6 @@
 // un-comment to use OpenAI API
-const { GetItems } = require('../../util/apiv2');
-// const chat = require("../../util/api");
+// const { GetItems } = require('../../util/apiv2');
+const chat = require("../../util/api");
 module.exports = {
   name: "chat",
   category: "talk",
@@ -10,13 +10,13 @@ module.exports = {
   run: async function ({ client, message, prefix, args }) {
     let chatMess = message.content.replace(prefix + this.name + " ", "");
     // Old API
-    // const response = await chat.GetItems(
-    //   message.author.id,
-    //   chatMess,
-    //   message.author.username,
-    //   client.user.username,
-    //   chatMess
-    // );
+    const response = await chat.GetItems(
+      message.author.id,
+      chatMess,
+      message.author.username,
+      client.user.username,
+      chatMess
+    );
     if (
       message.content == prefix + this.name + " " ||
       message.content == prefix + this.name
@@ -24,22 +24,22 @@ module.exports = {
       message.channel.send("Bạn cần nhập nội dung").catch((err) => {console.log(err)});
     } 
     // Old API
-    // else {
-    //   message.channel.send(response.data).catch((err) => {
-    //     console.log(err);
-    //     message.reply("Error something went wrong :(((.").catch((err) => {console.log(err)});
-    //   });
-    // }
+    else {
+      message.channel.send(response.data).catch((err) => {
+        console.log(err);
+        message.reply("Error something went wrong :(((.").catch((err) => {console.log(err)});
+      });
+    }
       // Using OpenAI API
-      else {
-          GetItems(chatMess)
-          .then(outputText => {
-              message.channel.send(`${outputText}`).catch((err) => {console.log(err)});
-          })
-          .catch(error => {
-              console.error(error);
-              message.reply('Error something went wrong :(((.').catch((err) => {console.log(err)});
-          });
-      }
+      // else {
+      //     GetItems(chatMess)
+      //     .then(outputText => {
+      //         message.channel.send(`${outputText}`).catch((err) => {console.log(err)});
+      //     })
+      //     .catch(error => {
+      //         console.error(error);
+      //         message.reply('Error something went wrong :(((.').catch((err) => {console.log(err)});
+      //     });
+      // }
   },
 };
--- slashcommands/talk/chat.js
+++ slashcommands/talk/chat.js
@@ -1,22 +1,22 @@
-// const chat = require("../../util/api");
+const chat = require("../../util/api");
 // un-comment to use OpenAI API
-const { GetItems } = require('../../util/apiv2');
+// const { GetItems } = require('../../util/apiv2');
 
 const run = async ({ client, interaction, prefix }) => {
   await interaction.deferReply();
   let message = interaction.options.getString("message");
   // Old public API
-  // const response = await chat.GetItems(
-  //   interaction.user.id,
-  //   message,
-  //   interaction.user.username,
-  //   client.user.username,
-  //   message
-  // );
-  // interaction.editReply(response.data).catch((err) => {
-  //   console.log(err);
-  //   interaction.editReply("Error something went wrong :(((.").catch((err) => {console.log(err)});
-  // });
+  const response = await chat.GetItems(
+    interaction.user.id,
+    message,
+    interaction.user.username,
+    client.user.username,
+    message
+  );
+  interaction.editReply(response.data).catch((err) => {
+    console.log(err);
+    interaction.editReply("Error something went wrong :(((.").catch((err) => {console.log(err)});
+  });
   // Using OpenAI API
     GetItems(message)
     .then(outputText => {
