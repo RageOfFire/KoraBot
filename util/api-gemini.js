0039009472f21f12d57987c37f4c7d31963d24e6
RageOfFire <buihongson453@gmail.com>
2024-06-28 22:39:59 +0700

    now build-in chat session for text-base game

--- util/api-gemini.js
+++ util/api-gemini.js
@@ -1,10 +1,50 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require("@google/generative-ai");
+const { parts } = require("./gameplay.js")
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API);
 
-async function GetItems(message) {
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest"});
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
 
-  const result = await model.generateContent(message);
+async function GetItems(message) {
+
+  const generationConfig = {
+    temperature: 2,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 200,
+    responseMimeType: "text/plain",
+  };
+
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    },
+  ];
+
+  const chatSession = model.startChat({
+    generationConfig,
+    safetySettings,
+    history: [
+      {
+        role: "user",
+        parts: parts
+      },
+    ],
+  });
+
+  const result = await chatSession.sendMessage(message);
   return result.response.text();
 }
 
